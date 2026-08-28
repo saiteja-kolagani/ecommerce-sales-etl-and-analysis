d63b68e67ca382391e937f529d086aaa4eebea22
Sai Teja Koagani <saiteja.kolagani@gmail.com>
2026-08-28 12:00:27 +0530

    Configured Snowflake Connection

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from config.schema import EXPECTED_SCHEMAS
 from src.transform_data import transform_data
 from src.validate_transformed_data import validate_transformed_data
 from src.save_processed_data import save_processed_data
+from src.utils.snowflake_connection import get_snowflake_connection
 
 def main():
+    conection = None
+
     try:
         logger.info("Starting the pipeline...")
 
@@ -41,10 +44,19 @@ def main():
 
         logger.info("Pipeline completed successfully without errors.")
 
+        # Step 6: Establish Snowflake connection
+        logger.info("Establishing Snowflake connection...")
+        connection = get_snowflake_connection()
+        logger.info("Snowflake connection established successfully.")
+
     except Exception:
         logger.exception("Pipeline failed due to an error.")
 
     finally:
+        if connection:
+            connection.close()
+            logger.info("Snowflake connection closed.")
+        
         logger.info("Pipeline execution completed.")
 
 if __name__ == '__main__':
